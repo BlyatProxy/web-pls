9f93e4369d0a6b1c308f32da3e402a3af19f5a35
BlyatProxy <altaltakun@gmail.com>
2024-12-05 19:27:11 +0800

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['api.microlink.io', 'images-eds-ssl.xboxlive.com']
+  }
+};
 
 export default nextConfig;
